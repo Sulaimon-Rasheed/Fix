c83ecb6a0dc7e0ca1f9248ea171ffc42ca5fc3c2
Sulaimon <maito4me@gmail.com>
2023-10-17 18:23:51 +0100

    edit to json

--- test/welcome.test.js
+++ test/welcome.test.js
@@ -8,7 +8,7 @@ describe("Index route", ()=>{
         expect(response.status).toEqual(200)
     })
 
-    it("should resond with an HTML page if the route does not exist", async()=>{
+    it("should respond with an HTML page if the route does not exist", async()=>{
         const response = await supertest(app).get("/undefined").set("content-type", "text/html")
         expect(response.status).toEqual(404)
     })
